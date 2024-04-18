edf8cfcf37029462e1d6c3541cdb1b55902d2153
GISANGYU <144764856+GISANGYU@users.noreply.github.com>
2024-04-19 07:27:24 +0900

    reload
    again

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    // 초기에 사용자의 높이값을 저장할 변수
-    var initialViewportHeight = $(window).height();
 
-    $(".background").css("height", initialViewportHeight + "px");//초기 높이값을받아 배경 높이값을 변경되지않는 값으로 적용
 
     $(window).scroll(function () {
+        // 뷰포트의 높이를 가져옴
+        var viewportHeight = $(window).height();
         // 스크롤된 값 (뷰포트 높이로 나누어 백분율로 표시)
-        var scrollPercentage = ($(this).scrollTop() / initialViewportHeight) * 100;
+        var scrollPercentage = ($(this).scrollTop() / viewportHeight) * 100;
+
 
         console.log('스크롤된 백분율: ' + scrollPercentage + '%');
 
@@ -14,5 +14,30 @@ $(document).ready(function () {
 
         $(".background").css("opacity", opacity + "%"); // opacity 값을 적용
 
+        if (opacity < 0) {
+            $(".background").css("display", "none");
+        } else {
+            $(".background").css("display", "block");
+        }
     });
-});
+
+    $(".c_header").click(function () {
+        // 클릭한 헤더의 인덱스를 가져옵니다.
+        var index = $(this).index() / 2;
+        console.log(index)
+        // 클릭한 헤더의 다음 요소인 쇼를 토글합니다.
+        $(".c_show").eq(index).toggle();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+})
